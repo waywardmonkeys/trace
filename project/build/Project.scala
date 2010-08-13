da63a6be40a6be504bd411ca05a37e1e6a7565df
Bruce Mitchener <bruce.mitchener@gmail.com>
2010-08-13 15:55:06 +0700

    Start adding support for generating OSGi bundles from this code.

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,9 +1,22 @@
 import sbt._
 import de.element34.sbteclipsify._
+import com.weiglewilczek.bnd4sbt.BNDPlugin
+import com.weiglewilczek.bnd4sbt.ExecutionEnvironment._
 
-class Project(info: ProjectInfo) extends DefaultProject(info) with IdeaProject with Eclipsify {
+class Project(info: ProjectInfo) extends DefaultProject(info)
+  with IdeaProject
+  with Eclipsify
+  with BNDPlugin
+{
   val junitVersion = "4.8.1"
   val scalaTestVersion = "1.2"
+
+    // OSGi stuff
+  override lazy val bndExecutionEnvironment = Set(Java6)
+  override lazy val bndBundleVendor         = Some("Data Fueled (http://www.datafueled.com/)")
+  override lazy val bndBundleLicense        = Some("Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)")
+  override lazy val bndVersionPolicy        = Some("[$(@),$(@)]")
+  override lazy val bndExportPackage        = Seq("com.datafueled.trace.*;version=%s".format(projectVersion.value))
 
   val scalaTest = "org.scalatest" % "scalatest" % scalaTestVersion % "test"
   val junit = "junit" % "junit" % junitVersion % "test"
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,9 +1,13 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val repo = "GH-pages repo" at "http://mpeltonen.github.com/maven/"
+  val ideaRepo = "GH-pages repo" at "http://mpeltonen.github.com/maven/"
+  val bnd4sbtRepo = "aQute Maven Repository" at "http://www.aqute.biz/repo"
+
+  lazy val aquteModuleConfig = ModuleConfiguration("biz.aQuote", bnd4sbtRepo)
 
   lazy val idea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.1-SNAPSHOT"
   lazy val eclipse = "de.element34" % "sbt-eclipsify" % "0.6.0"
+  lazy val bnd4sbt = "com.weiglewilczek.bnd4sbt" % "bnd4sbt" % "1.0.0.RC6"
 }
 
